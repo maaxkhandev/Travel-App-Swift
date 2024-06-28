2e0269f0c04465128669acc7438ba1c045641eed
maazkhan2 <maazkhanswat02@gmail.com>
2024-06-28 12:22:52 +0500

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Maaz Khan on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
